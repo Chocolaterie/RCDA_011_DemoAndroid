2883feda1f8bb37a72ff09096804938d5c8f0ac8
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-07-26 14:02:45 +0200

    Demo on click

--- app/src/main/java/com/example/enidemo/DataBindingActivity.kt
+++ app/src/main/java/com/example/enidemo/DataBindingActivity.kt
@@ -7,6 +7,9 @@ import com.example.enidemo.databinding.ActivityDataBindingBinding
 
 class DataBindingActivity : AppCompatActivity() {
 
+    // En membre de classe
+    var person : Person = Person("Vincent", "Le Goat");
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,9 +17,22 @@ class DataBindingActivity : AppCompatActivity() {
         var myView = DataBindingUtil.setContentView<ActivityDataBindingBinding>(this, R.layout.activity_data_binding)
 
         // Dans ma vue => la person c'est untel
-        myView.person = Person("Vincent", "Le Goat")
+        myView.person = person
 
         // Je change le texte de mon bouton
         myView.myButton.text = "Mon Bouton Morgan"
+
+        // je définie le comportement du clique du bouton de la vue
+        myView.myButton.setOnClickListener {
+            // Modifie la valeur d'une donné de la vue ?
+            //myView.person.firstname = "Toto"
+
+            // Je met à jour l'attribut d'une instance
+            person.firstname = "Morgan";
+
+            // J'appel le setter de la vue pour enclencher le rafaichissement
+            myView.person = person // Met toi à jour
+
+        }
     }
 }
