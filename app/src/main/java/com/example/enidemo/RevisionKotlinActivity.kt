98f31a20b13375419912ff5a24ffc5b7d83b05e3
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-07-27 10:23:05 +0200

    Push

--- app/src/main/java/com/example/enidemo/RevisionKotlinActivity.kt
+++ app/src/main/java/com/example/enidemo/RevisionKotlinActivity.kt
@@ -33,9 +33,6 @@ class PersonTest (var firstname : String,  var lastname : String, var age : Int 
 
     // toute les fonctions statics
     companion object {
-        fun getTVA() : Double{
-            return 19.6
-        }
 
         fun getToto() : String {
             return "Toto"
@@ -52,52 +49,5 @@ class RevisionKotlinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_revision_kotlin)
 
-        test();
-    }
-
-    fun test(){
-        // var = ecriture/lecture
-        var age = 12
-        age = 15
-
-        // val = constante
-        val age2 = 16
-        // age2 = 52 // impossible car lecture seule
-
-        var age4 : Int
-        var note : Double
-        var lastname : String
-
-        var notes = ArrayList<Double>()
-
-        notes.average()
-
-        var noteA = 12
-        var noteB = 20
-
-        noteA++
-
-        noteA + noteB
-
-        // Test static
-        Log.i("Kotlin revision", String.format("La TVA : %f", PersonTest.getTVA()))
-
-        // Test surcharge d'operateur
-        var isaac = PersonTest("isaac", "test");
-        var morgan = PersonTest("morgan", "test");
-
-        var resultat = isaac + morgan;
-        Log.i("Kotlin revision", "Resultat de l'addition: " + resultat.firstname)
-
-        var resultat2 = isaac - morgan;
-        Log.i("Kotlin revision", "Resultat de la soustraction: " + resultat2.firstname)
-
-        // Liste de deux persons
-        var persons = arrayListOf<PersonTest>(isaac, morgan)
-
-        var personEnBase = PersonTest("isaac", "test") // nouvelle adresse mémoire
-
-        persons.contains(personEnBase)
-        val index = persons.indexOf(personEnBase)
     }
 }
