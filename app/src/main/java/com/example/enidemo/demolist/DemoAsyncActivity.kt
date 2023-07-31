44933b006a9596c2eaf5b08b17edacaca5c1cea1
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-07-31 16:45:20 +0200

    Demo Handler Custom

--- app/src/main/java/com/example/enidemo/demolist/DemoAsyncActivity.kt
+++ app/src/main/java/com/example/enidemo/demolist/DemoAsyncActivity.kt
@@ -5,8 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.Message
 import android.util.Log
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.enidemo.MainActivity
 import com.example.enidemo.Person
@@ -16,7 +16,7 @@ class DemoAsyncActivity : AppCompatActivity() {
 
     lateinit var progressDialog : ProgressDialog;
 
-    override fun onCreate(savedInstanceState: Bundle?): Boolean {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo_async)
 
@@ -26,45 +26,51 @@ class DemoAsyncActivity : AppCompatActivity() {
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Chargement")
         progressDialog.setMessage("Chargement en cours")
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
 
         // Affiche le progress bar
         progressDialog.show()
 
-        // Instancier un handler
-        var handler = Handler(Handler.Callback {
-
-            progressDialog.setMessage(it.toString())
-
-            true
-        })
+        // Notre handler
+        var handler = RegisterHandler(progressDialog)
 
         // Creer une tâche
         var thread1 = Thread(
             Runnable {
                 // Appel le web service
-                Thread.sleep(500)
-
-                // Envoyer message Etape 1
-                var message = Message()
-                message.obj = "Etape 1"
-                handler.sendMessage(message)
+                handler.onStepProgress(0.0, "Initialisation...")
 
                 // Attendre 1 seconde
                 Thread.sleep(1000)
+                handler.onStepProgress(0.2,"Envoi des données en cours ...")
 
-                // Envoyer message Etape 2
-                var message2 = Message()
-                message2.obj = "Etape 2"
-                handler.sendMessage(message2)
+                // Attendre 1 seconde
+                Thread.sleep(1500)
+                handler.onStepProgress(0.8,"Insertion en base ...")
+
+                // Attendre 1 seconde
+                Thread.sleep(500)
+                handler.onStepProgress(0.9,"Validation en cours ...")
 
                 Log.i("EniDemoThread", person.firstname)
 
-                // Fermer la progress bar
-                progressDialog.dismiss()
+                Thread.sleep(500)
+                handler.onStepProgress(1.0,"Complete")
             }
         )
         // Lancer le thread
         thread1.start()
     }
 
+    class RegisterHandler(var progressDialog: ProgressDialog) {
+        fun onStepProgress(weight: Double, message: String){
+            progressDialog.setMessage(message)
+            progressDialog.progress = (weight * 100.0).toInt()
+
+            if (weight >= 1.0) {
+                // Fermer la progress bar
+                progressDialog.dismiss()
+            }
+        }
+    }
 }
