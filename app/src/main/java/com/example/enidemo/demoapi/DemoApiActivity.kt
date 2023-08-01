77e940a3f23be5b01216894368e493b0afc409a9
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-08-01 10:31:59 +0200

    Demo ApI

--- app/src/main/java/com/example/enidemo/demoapi/DemoApiActivity.kt
+++ app/src/main/java/com/example/enidemo/demoapi/DemoApiActivity.kt
@@ -2,11 +2,30 @@ package com.example.enidemo.demoapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.lifecycle.lifecycleScope
 import com.example.enidemo.R
+import kotlinx.coroutines.launch
 
 class DemoApiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo_api)
+
+        // le clique du bouton
+        findViewById<Button>(R.id.btnAppelApi).setOnClickListener {
+            appelApi()
+        }
+    }
+    fun appelApi(){
+        // Appel api
+        lifecycleScope.launch {
+            // Appel le service
+            val joke = ChuckApi.retrofitService.getRandomJoke()
+
+            // Afficher la blague dans la console
+            Log.i("DemoApi", String.format("la blague est : %s", joke.message))
+        }
     }
 }
